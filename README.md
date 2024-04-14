b1755e58d587fb322ae721d4a68f1bd423c9fcc7
HJ Hubeek <hjhubeek@gmail.com>
2024-04-14 20:00:51 +0200

    first test added to save a message in the db.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Without Spring boot.
 Basic soap service that handles the crud around Message Entity stored in MySql database.
+
+```bash
+docker run --name mysql -e MYSQL_USER=user -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=databasename -p 3306:3306 -d mysql/mysql-server
+```
---
+++ src/test/java/nl/appall/soapServer/MessageServiceTest.java
@@ -0,0 +1,62 @@
+package nl.appall.soapServer;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MessageServiceTest {
+    private Session session;
+
+    @BeforeEach
+    void setUp() {
+        createSessionDb();
+
+        cleanDb();
+    }
+
+    private void createSessionDb() {
+        // Open a session and start a transaction
+        session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+    }
+
+    private void cleanDb() {
+        // Clean up the database
+        session.createSQLQuery("DELETE FROM messages").executeUpdate();
+
+        // Commit transaction
+        session.getTransaction().commit();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (session != null) {
+            session.close();
+        }
+//        createSessionDb();
+//        cleanDb();
+//        session.close();
+    }
+
+    @Test
+    public void testSaveMessage() {
+        // Open a new transaction for the test
+        session.beginTransaction();
+        try {
+            MessageServiceImpl service = new MessageServiceImpl();
+            String content = "Hello, world!";
+            Message message = service.saveMessage(content);
+            assertEquals(content, message.getContent(), "The message content should match the input");
+
+            // Commit transaction
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            // Rollback in case of an exception
+            session.getTransaction().rollback();
+            throw e; // rethrow the exception to fail the test
+        }
+    }
+}
